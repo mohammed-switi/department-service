e1d248c13fcee72025d41a6f88f83534957f04bb
mohammed-switi <switi.2003@gmail.com>
2025-05-17 18:18:21 +0300

    removed some warnings, and also made the ci-cd pipeline to run only on pull requests

--- src/main/java/com/lab/department_service/DepartmentRepository.java
+++ src/main/java/com/lab/department_service/DepartmentRepository.java
@@ -1,11 +1,9 @@
 package com.lab.department_service;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
-     Optional<Department> findByName(String name);
 
 }
